5774cddbe9f4c394a49fe3fb7938674e0c39c7fe
OniDem1 <spaindecraft17@gmail.com>
2023-06-23 11:40:47 +0700

    Добавьте файлы проекта.

---
+++ ToDoProgramm/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ToDoProgramm.Models;
+using ToDoProgramm.Services;
+
+namespace ToDoProgramm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly string PATH = $"{Environment.CurrentDirectory}\\todoData.json";
+        private BindingList<TodoModel> _todoData;
+        private FileIOService _fileIOService;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _fileIOService = new FileIOService(PATH);
+           try
+            {
+                _todoData = _fileIOService.LoadData();
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show(ex.Message);
+                Close();
+            }
+
+            dgtodo.ItemsSource = _todoData;
+            _todoData.ListChanged += _todoDataList_ListChanged!;
+        }
+        private void _todoDataList_ListChanged(object sender, ListChangedEventArgs e)
+        {
+
+            if (e.ListChangedType == ListChangedType.ItemAdded || e.ListChangedType == ListChangedType.ItemDeleted || e.ListChangedType == ListChangedType.ItemChanged)
+            {
+                try
+                {
+                    _fileIOService.SaveData(sender);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    Close();
+                }
+            }
+
+        }
+    }
+}
---
+++ ToDoProgramm/Models/TodoModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoProgramm.Models
+{
+    class TodoModel : INotifyPropertyChanged
+    {
+        public string Creation { get; set; } = DateTime.Now.ToString("dd.MM HH:mm");
+
+        private bool _isDone;
+
+        private string _Task;
+
+
+
+        public bool IsDone
+        {
+            get
+            {
+                return _isDone;
+            }
+            set
+            {
+                if (_isDone == value)
+                    return;
+                _isDone = value;
+                OnPropertyChanged("IsDone");
+            }
+        }
+
+        public string Task
+        {
+            get
+            {
+                return _Task;
+            }
+            set
+            {
+                if (_Task == value)
+                    return;
+                _Task = value;
+                OnPropertyChanged("Task");
+            }
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
